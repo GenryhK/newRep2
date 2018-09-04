e750eaf63af5a713ff9dfdfa4bf95b1f4ae7010d
Alex <a.opanasenkov@gmail.com>
2018-09-04 16:53:30 +0300

    add file

---
+++ massiv.js
@@ -0,0 +1,21 @@
+<script>
+    var value = ['Лимон', 'Яблоко', 'Апельсин' ];
+
+    alert ( value.indexOf('Яблоко') );
+
+    var arr = value.join(';');
+
+    for (i=0; i<arr.length; i++);
+
+    alert( 'Яблоко' );
+    alert( 'такого слова нет' );
+    value.push('Гранат');
+    alert( value );
+    
+    var value2 = prompt (' Добавить любое слово в массив, ');
+    alert( 'Какое слово?' );
+    for (i=0; i<value.length; i++);
+    alert( 'Такое слово есть' );
+    alert( 'Такого слова нет' );
+
+</script>
